d5aaa215e59dd0e016ea45c86ce1ac9732dd690c
Roman <romatka4enko2@gmail.com>
2025-09-03 22:54:41 +0300

    start creating a pyramid generator

--- JavaScript Algorithms and Data Structures/Introductory JavaScript/arrays.js
+++ JavaScript Algorithms and Data Structures/Introductory JavaScript/arrays.js
@@ -1,5 +1,22 @@
-let rows = ["Naomi", "Quincy", "CamperChan"];
-rows.push("freeCodeCamp");
-let popped = rows.pop();
-console.log(rows);
-console.log(popped);
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(){
+  
+}
+
+const call = padRow();
+console.log(call);
+
+for(let i = 0; i < count; i=i+1){
+    rows.push(character.repeat(i+1));
+}
+
+
+/*
+let result = "";
+console.log(result);
+for (const row of rows){
+    result = result + row + "\n";
+*/
