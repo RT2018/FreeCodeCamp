024315e7fb645df450781b24a7f70008187ee650
Roman <romatka4enko2@gmail.com>
2025-09-03 22:02:15 +0300

    change array

--- JavaScript Algorithms and Data Structures/Introductory JavaScript/arrays.js
+++ JavaScript Algorithms and Data Structures/Introductory JavaScript/arrays.js
@@ -1,4 +1,4 @@
-let rows = ["Naomi", "Quincy", "CamperChan"];
-console.log(rows[0])
-rows[rows.length-1] = 10;
-console.log(rows);
+let cities = ["London", "New York", "Mumbai"];
+console.log(cities);
+cities[cities.length-1] = "Mexico City";
+console.log(cities);
